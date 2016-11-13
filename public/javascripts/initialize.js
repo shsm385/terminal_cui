e8ff088512ff623b03caff2b6a11ebc5cc9cc568
shsm385 <shsm385@gmail.com>
2016-11-14 03:59:14 +0900

    add function csv

--- public/javascripts/initialize.js
+++ public/javascripts/initialize.js
@@ -15,3 +15,21 @@ util.getDocHeight = function() {
         Math.max(D.body.clientHeight, D.documentElement.clientHeight)
     );
 };
+
+function getCSV() {
+    var req = new XMLHttpRequest();
+    req.open("get", "./public/models/csv/osaka.csv", true);
+    req.send(null);
+    req.onload = function() {
+        convertCSVtoArray(req.responseText);
+    };
+}
+
+function convertCSVtoArray(str) {
+    var result = [];
+    var tmp = str.split("\n");
+    for (var i = 0; i < tmp.length; ++i) {
+        result[i] = tmp[i].split(',');
+    }
+    console.log(result);
+}
