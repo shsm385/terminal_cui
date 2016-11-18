019759117d2abdc7faaf5a3041e908529cd26145
shsm385 <shsm385@gmail.com>
2016-11-19 03:57:26 +0900

    add create object

--- public/javascripts/initialize.js
+++ public/javascripts/initialize.js
@@ -19,36 +19,45 @@ util.getDocHeight = function() {
 
 // aouthor shimada
 function csv2Array(filePath) {
-	var csvData = [];
-	var data = new XMLHttpRequest();
-	data.open("GET", filePath, false);
-	data.send(null);
+    var csvData = [];
+    var data = new XMLHttpRequest();
+    data.open("GET", filePath, false);
+    data.send(null);
 
-	var LF = String.fromCharCode(10);
-	var lines = data.responseText.split(LF);
-	for (var i = 0; i < lines.length;++i) {
-		var cells = lines[i].split(",");
-		if( cells.length != 1 ) {
-			csvData.push(cells);
-		}
-	}
-	return csvData;
+    var LF = String.fromCharCode(10);
+    var lines = data.responseText.split(LF);
+    for (var i = 0; i < lines.length; ++i) {
+        var cells = lines[i].split(",");
+        if (cells.length != 1) {
+            csvData.push(cells);
+        }
+    }
+    return csvData;
 }
 
 // aouthor shimada
-function createPath(str){
-    var path = '/';
-    path.add('aaa');
-    console.log(path);
-    var temp ;
-    for (var i = 1; i < 4; i++){
-      for(var j = 1; j < 4; j++){
-        if(i === 1){
-          // path.appendChild(str[1][j]);
+function createPath(str) {
+    var dir = {
+        root: {
+            name: '/',
+            osaka: {
+                name: "大阪",
+            }
         }
-      }
+    };
+    var temp = {};
+    for (var i = 0; i < 4; i++) {
+        for (var j = 2; j < 4; j++) {
+            if(j == 3){
+                dir.root.osaka[str[i][j-1]] = temp;
+                dir.root.osaka[str[i][j-1]][str[i][j]] = {};
+                dir.root.osaka[str[i][j-1]][str[i][j]].name = str[i][j];
+            }
+        }
     }
-    return path;
+      console.log(dir);
+    return dir;
 }
 
-console.log(createPath(csv2Array("./public/models/csv/osaka.csv")));
+// Path作成の実行
+createPath(csv2Array("./public/models/csv/osaka.csv"));
